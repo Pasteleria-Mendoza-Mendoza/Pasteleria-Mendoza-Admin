ef8d1af44c3ca52d23b7a22aa769a2974ea4e44f
Leonel Alexander <leo.gise02@outlook.com>
2024-01-20 23:32:51 -0600

    Creación de dto de admin

---
+++ ProyectAdmin/ProyectAdmin.BL/AdminBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectAdmin.BL;
+using ProyectAdmin.BL.Interfaces;
+using ProyectAdmin.EN;
+using ProyectAdmin.EN.Interfaces;
+
+namespace ProyectAdmin.BL
+{
+    public class AdminBL : IAdminBL
+    {
+    }
+}
