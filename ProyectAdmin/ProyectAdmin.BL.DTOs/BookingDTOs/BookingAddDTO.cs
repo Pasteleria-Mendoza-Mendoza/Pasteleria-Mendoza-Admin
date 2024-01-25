c4bc0024be043c1afd3fe36b5271f3512d631a05
Leonel Alexander <leo.gise02@outlook.com>
2024-01-24 18:37:07 -0600

    Cambios

---
+++ ProyectAdmin/ProyectAdmin.BL.DTOs/BookingDTOs/BookingAddDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.BL.DTOs.BookingDTOs
+{
+    public class BookingAddDTO
+    {
+        public string CustomerName { get; set; }
+        public int ContactNumber { get; set; }
+        public int CakeQuantity { get; set; }
+        public string CakeDimensions { get; set; }
+        public string CakeDedication { get; set; }
+        public int ReservationShipping {  get; set; }
+        public int ReservationDate { get; set; }
+        public int CakeCost { get; set; }
+    }
+}
--- ProyectAdmin/ProyectAdmin.BL.Interfaces/IBookingBL.cs
+++ ProyectAdmin/ProyectAdmin.BL.Interfaces/IBookingBL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectAdmin.BL.DTOs.BookingDTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
