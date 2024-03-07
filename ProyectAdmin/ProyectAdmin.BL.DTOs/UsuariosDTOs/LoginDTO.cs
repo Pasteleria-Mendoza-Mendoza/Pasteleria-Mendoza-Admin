e76fd088315226da06e184b0edc4b124ff07bdaf
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 23:57:18 -0600

    Creacion de LoginDTO

---
+++ ProyectAdmin/ProyectAdmin.BL.DTOs/UsuariosDTOs/LoginDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.BL.DTOs.UsuariosDTOs
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Nombre de Usurios requerido")]
+        public string login { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
