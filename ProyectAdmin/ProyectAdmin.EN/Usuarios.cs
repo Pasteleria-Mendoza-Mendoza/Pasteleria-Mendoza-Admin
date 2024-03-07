87945ae305f18791ba21ee49cf0c3abc8d29de86
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 23:51:14 -0600

    Creacion de entidad de Usuarios

---
+++ ProyectAdmin/ProyectAdmin.EN/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectAdmin.EN
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("Rol")]
+        public int RolId { get; set; }
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public byte Estado { get; set; }
+        public Rol Rol { get; set; }
+    }
+
+    public enum EstadoUsuario
+    {
+        Activo = 1,
+        Inactivo = 2
+    }
+}
