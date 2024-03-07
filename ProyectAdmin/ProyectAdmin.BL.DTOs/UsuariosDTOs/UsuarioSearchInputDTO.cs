b3dde5e83be287f75fc94d91cb6c0b37f743c7a0
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 23:58:59 -0600

    Cracion de UsuarioSearchInputDTO

---
+++ ProyectAdmin/ProyectAdmin.BL.DTOs/UsuariosDTOs/UsuarioSearchInputDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.BL.DTOs.UsuariosDTOs
+{
+    public class UsuarioSearchInputDTO
+    {
+        public string? NombreLike { get; set; }
+        public string? TelefonoLike { get; set; }
+    }
+}
