6a6a9c47fc9459ad3b94accf09a5aab87d38ca13
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 22:23:15 -0600

    Creacion de entidad Rol

---
+++ ProyectAdmin/ProyectAdmin.EN/Rol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.EN
+{
+    public class Rol
+    {
+        public int RolId { get; set; }
+        public string Nombre { get; set; }
+        public byte Estado { get; set; }
+    }
+}
