f2241e21977e2a9c9b3a182ec96b09ad2144016b
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-07 00:06:59 -0600

    Creacion de IUsuarioBL

---
+++ ProyectAdmin/ProyectAdmin.BL.Interfaces/IUsuarioBL.cs
@@ -0,0 +1,22 @@
+﻿using ProyectAdmin.BL.DTOs.UsuariosDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.BL.Interfaces
+{
+    public interface IUsuarioBL
+    {
+        Task<int> Create(CreateUsuarioDTO pUser);
+
+        Task<int> Update(UsuarioUpdateDTO pUser);
+
+        Task<int> Delete(int Id);
+
+        Task<UsuarioSearchOutputDTO> GetById(int Id);
+
+        Task<List<UsuarioSearchOutputDTO>> Search(UsuarioSearchInputDTO pUser);
+    }
+}
