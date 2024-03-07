5f6fd1e863705c4195678689bd333b79f90da73a
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-07 00:03:13 -0600

    Modificacion en UsuarioSearchOutputDTO

--- ProyectAdmin/ProyectAdmin.BL.DTOs/UsuariosDTOs/UsuarioSearchOutputDTO.cs
+++ ProyectAdmin/ProyectAdmin.BL.DTOs/UsuariosDTOs/UsuarioSearchOutputDTO.cs
@@ -13,18 +13,14 @@ namespace ProyectAdmin.BL.DTOs.UsuariosDTOs
         public int UsuarioId { get; set; }
         [DisplayName("Rol")]
         public int RolId { get; set; }
-        [DisplayName("Sucursal")]
-        public int SucursalesId { get; set; }
+
         [DisplayName("NombreRol")]
         public string NombreRol { get; set; }
         [DisplayName("Nombre")]
         public string Nombre { get; set; }
         [DisplayName("Apellido")]
         public string Apellido { get; set; }
-        [DisplayName("Telefono")]
-        public string Telefono { get; set; }
-        [DisplayName("DUI")]
-        public string Dui { get; set; }
+
         [DisplayName("Usuario")]
         public string? Usuario { get; set; }
         [DisplayName("Correo")]
@@ -32,7 +28,5 @@ namespace ProyectAdmin.BL.DTOs.UsuariosDTOs
         [DisplayName("Estado")]
         public byte Estado { get; set; }
 
-        ////Almacena el nombre del rol
-        // public string NombreRol { get; set; }
     }
 }
