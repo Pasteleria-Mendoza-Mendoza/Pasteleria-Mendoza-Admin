5a705e7b5fb9487f500921e00efccbba6e5b606a
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 23:52:51 -0600

    Creacion de Interfaz dal usuario

---
+++ ProyectAdmin/ProyectAdmin.EN.Interfaces/IUsuarioDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.EN.Interfaces
+{
+    public interface IUsuarioDAL
+    {
+        public void Create(Usuarios pUsuarios);
+        public void Update(Usuarios pUsuarios);
+        public void Delete(Usuarios pUsuarios);
+        public Task<Usuarios> GetById(int Id);
+        public Task<List<Usuarios>> Search(Usuarios pUsuarios);
+
+    }
+}
