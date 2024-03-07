690d1ca9524ee470fdbbb9be77a9cfa6b231c446
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 23:58:17 -0600

    Creacion de SecurityDTO

---
+++ ProyectAdmin/ProyectAdmin.BL.DTOs/UsuariosDTOs/SecurityDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.BL.DTOs.UsuariosDTOs
+{
+    public class SecurityDTO
+    {
+        public int UsuarioId { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Login { get; set; }
+        public int RolId { get; set; }
+        public string NombreRol { get; set; }
+
+    }
+}
