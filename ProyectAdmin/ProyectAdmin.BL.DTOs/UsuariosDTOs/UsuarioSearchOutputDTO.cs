5dd9ccee2f389e75a8ebd91466709e3a915bdf8a
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 23:59:58 -0600

    Creacion de UsuarioSearchOutputDTO

---
+++ ProyectAdmin/ProyectAdmin.BL.DTOs/UsuariosDTOs/UsuarioSearchOutputDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.BL.DTOs.UsuariosDTOs
+{
+    public class UsuarioSearchOutputDTO
+    {
+        [DisplayName("Id Usuario")]
+        public int UsuarioId { get; set; }
+        [DisplayName("Rol")]
+        public int RolId { get; set; }
+        [DisplayName("Sucursal")]
+        public int SucursalesId { get; set; }
+        [DisplayName("NombreRol")]
+        public string NombreRol { get; set; }
+        [DisplayName("Nombre")]
+        public string Nombre { get; set; }
+        [DisplayName("Apellido")]
+        public string Apellido { get; set; }
+        [DisplayName("Telefono")]
+        public string Telefono { get; set; }
+        [DisplayName("DUI")]
+        public string Dui { get; set; }
+        [DisplayName("Usuario")]
+        public string? Usuario { get; set; }
+        [DisplayName("Correo")]
+        public string? Correo { get; set; }
+        [DisplayName("Estado")]
+        public byte Estado { get; set; }
+
+        ////Almacena el nombre del rol
+        // public string NombreRol { get; set; }
+    }
+}
