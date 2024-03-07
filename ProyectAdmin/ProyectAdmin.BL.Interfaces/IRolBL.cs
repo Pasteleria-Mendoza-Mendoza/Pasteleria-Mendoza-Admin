18f79c1a36aba45724e3b0a399efcb424e0fc118
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2024-03-06 22:26:36 -0600

    Creacion de Interfaces de Rol

---
+++ ProyectAdmin/ProyectAdmin.BL.Interfaces/IRolBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.BL.Interfaces
+{
+    public interface IRolBL
+    {
+        Task<int> Create(RolInputDTO pRol);
+
+        Task<int> Update(RolInputDTO pRol);
+
+        Task<int> Delete(int id);
+
+        Task<RolSearchingOutputDTO> GetById(int id);
+
+        Task<List<RolSearchingOutputDTO>> Search(RolSearchingInputDTO pRol);
+    }
+}
---
+++ ProyectAdmin/ProyectAdmin.EN.Interfaces/IRolDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAdmin.EN.Interfaces
+{
+    public void Create(Rol pRol);
+    public void Update(Rol pRol);
+    public void Delete(Rol pRol);
+    public Task<Rol> GetById(int Id);
+    public Task<List<Rol>> Search(Rol pRol);
+    public Task<List<Rol>> GetAll();
+}
